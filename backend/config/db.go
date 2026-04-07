e7a83d991a1f6edc5ebb5730157f3993c0bba948
Ridwan Taufik <ridwan1998taufik@gmail.com>
2026-04-07 15:51:23 +0700

    add db config and env

---
+++ backend/config/db.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("gagal terhubung ke db:", err)
+	}
+
+	DB = db
+	log.Println("db connected")
+}
